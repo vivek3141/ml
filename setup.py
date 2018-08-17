f002282611b592944150390ff69571adcba31804
Vivek Verma <vivnps.verma@gmail.com>
2018-08-17 00:31:00 -0700

    Fixing a few things

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from setuptools import setup
 
 setup(
     name='ml-python',
-    version='0.0',
+    version='0.2',
     packages=['ml',],
     author="Vivek Verma",
     author_email="vivekverma3141@gmail.com",
     url="https://github.com/vivek3141/ml",
     license='MIT',
-    long_description=open('README.txt').read(),
+    description="The easiest way to do machine learning",
+    long_description=open("DESC.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
