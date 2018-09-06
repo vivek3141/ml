624410b4c76ea142bacd5726847277e37e079186
Vivek Verma <vivnps.verma@gmail.com>
2018-09-05 21:44:33 -0700

    Adding changes to CNN

--- ml/cnn/cnn2d.py
+++ ml/cnn/cnn2d.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
+from ml.activation import relu
 
-class CNN2D():
-    def __init__(self, layers, inp, out, kernel_size, activation, pool_size):
+
+class CNN2D:
+    def __init__(self, layers, inp, out, kernel_size, pool_size, filters, activation=relu, strides=2):
         self.input = inp
         self.output = out
         self.layers = layers
@@ -9,21 +11,30 @@ class CNN2D():
         self.pool = None
         self.kernel_size = kernel_size
         self.activation = activation
+        self.filters = filters
+        self.strides = strides
         self.pool_size = pool_size
 
-    def create_model(self, data, labels, mode):
+    def _create_model(self, data, labels, mode):
         size = [-1, len(data), len(data[0]), 1]
         inp = tf.reshape(data, size)
         self.conv = tf.layers.conv2d(
             inputs=inp,
-            filters=32,
+            filters=self.filters[0],
             kernel_size=self.kernel_size,
             padding="same",
             activation=self.activation,
         )
-        self.pool = tf.layers.max_pooling2d(inputs=self.conv, pool_size=self.pool_size, strides=2)
-        for i in range(1, len(self.layers)-1):
-            pass
-
-
+        self.pool = tf.layers.max_pooling2d(inputs=self.conv, pool_size=self.pool_size, strides=self.strides)
+        for i in range(1, self.layers):
+            self.conv = tf.layers.conv2d(
+                inputs=self.pool,
+                filters=self.filters[i],
+                kernel_size=self.kernel_size,
+                padding="same",
+                activation=self.activation
+            )
+            self.pool = tf.layers.max_pooling2d(inputs=self.conv, pool_size=self.pool_size, strides=self.strides)
+        pool_flat = tf.reshape(self.pool, [-1, int(tf.size(self.pool))])
+        dense = tf.layers.dense(inputs=pool_flat, units=self.output)
 
