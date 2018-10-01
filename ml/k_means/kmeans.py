1aef7aa0fb813398c2435a6af9c61dfe4932d017
Vivek Verma <vivnps.verma@gmail.com>
2018-09-30 20:29:57 -0700

    Base KMeans

---
+++ ml/k_means/kmeans.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+
+
+class KMeans:
+    def __init__(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+
+    def train(self):
+        pass
