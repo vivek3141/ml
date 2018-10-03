87dea370e7e6946593c8e531da87838e046aef9c
Vivek Verma <vivnps.verma@gmail.com>
2018-10-02 21:32:56 -0700

    Cleaning setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ]
