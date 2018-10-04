91c27d5ae8a1c5b90bfbba628c58c26aed90a00f
Vivek Verma <vivnps.verma@gmail.com>
2018-10-03 21:21:50 -0700

    Formatting Examples

--- examples/k_means.py
+++ examples/k_means.py
@@ -3,7 +3,8 @@ from ml.random.kmeans import create_dataset
 from ml.graph.kmeans import plot
 
 N = 1000
+K = 4
 data, y, p = create_dataset(N)
-k = KMeans(4, N)
+k = KMeans(K, N)
 c, assign = k.fit(data, y, 50000)
 plot(p, assign, c)
