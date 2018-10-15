bccbf0b6e3446551786c63ce4fb841fdfd0662a9
Vivek Verma <vivnps.verma@gmail.com>
2018-10-14 20:19:58 -0700

    Formatting

--- examples/test.py
+++ examples/test.py
@@ -5,6 +5,7 @@ predicted = {'classes': 7,
                                         2.1170275e-10, 8.1893268e-06,
                                         1.6208340e-09, 2.7085177e-08,
                                         4.8949983e-15, 9.9999142e-01,
-                                        1.4859140e-10, 3.8220912e-07], dtype=np.float32)}
+                                        1.4859140e-10, 3.8220912e-07],
+                                       dtype=np.float32)}
 prob = list(predicted['probabilities'])
 print(prob.index(max(prob)))
