624fc417675c2763f9b393a7c6984ce805358305
Vivek Verma <vivnps.verma@gmail.com>
2018-10-28 21:04:24 -0700

    Fixing Import Warnings

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,3 +1,8 @@
+import warnings
+
+print("ignore")
+warnings.filterwarnings("ignore")
+
 from ml.k_means import *
 from ml.linear_regression import *
 from ml.logistic_regression import *
@@ -5,7 +10,3 @@ from ml.nn import *
 from ml.random import *
 from ml.activation import *
 from ml.cnn import *
-
-import warnings
-
-warnings.filterwarnings("always")
