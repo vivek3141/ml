b86af0f8a1b2eed23f8565c227549a4f9675a237
Vivek Verma <vivnps.verma@gmail.com>
2018-10-29 20:03:08 -0700

    Updating setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='ml-python',
-    version='1.8',
+    version='1.9',
     author="Vivek Verma",
     packages=find_packages(),
     author_email="vivekverma3141@gmail.com",
