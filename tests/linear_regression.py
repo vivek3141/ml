f5f2870968f61f3c55890899c92695d1b89c96e8
Vivek Verma <vivnps.verma@gmail.com>
2018-11-03 23:02:18 -0700

    Adding linear regression test file

---
+++ tests/linear_regression.py
@@ -0,0 +1,7 @@
+import numpy as np
+from ml.linear_regression import LinearRegression
+
+x = np.array(list(map(int, np.random.random(2))))
+y = np.array(list(map(int, np.random.random(2))))
+l = LinearRegression()
+l.fit(data=x, labels=y, graph=True)
