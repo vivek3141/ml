fdcf50dd5863a09629f0980406b713c8b0f307bd
Vivek Verma <vivnps.verma@gmail.com>
2018-11-03 23:05:11 -0700

    Adding logistic regression test

--- tests/logistic_regression.py
+++ tests/logistic_regression.py
@@ -1,7 +1,7 @@
 from ml.logistic_regression import LogisticRegression
 from ml.random import yx2features
 
-data, labels = yx2features(number=3)
+data, labels = yx2features(number=2)
 
 lr = LogisticRegression(features=2)
 lr.fit(data=data, labels=labels, graph=True, lr=0.01, steps=200, to_print=False)
