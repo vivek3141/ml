d3a90cb8ed9b1a0802c4d6fff28507aed2d221f1
Vivek Verma <vivnps.verma@gmail.com>
2018-11-03 23:09:14 -0700

    Adding NN test

--- tests/neuralnetwork.py
+++ tests/neuralnetwork.py
@@ -1,10 +1,13 @@
 from ml.nn import NeuralNetwork
 from ml.activation import relu
 import pickle
+import numpy as np
+
 with open("label.txt", "rb") as f:
     label = pickle.load(f)
 with open("data.txt", "rb") as f:
     image = pickle.load(f)
+
 n = NeuralNetwork(
     layers=[20],
     inp=784,
@@ -12,6 +15,5 @@ n = NeuralNetwork(
     out=10,
 )
 
-n.fit(51, data=mnist.train.images, labels=mnist.train.labels, lr=0.0001, graph=True)
-n.test(mnist.test.images, mnist.test.labels)
-
+n.fit(1, data=np.array([image, image]), labels=np.array([[0] * 10, [0] * 10]), lr=0.001)
+n.test(np.array([image]), np.array([label], dtype=np.int32))
