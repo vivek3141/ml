927b53c1349da76115775e902e3e5af10707011b
Vivek Verma <vivnps.verma@gmail.com>
2018-11-03 22:53:10 -0700

    Fixing Gitignore

--- tests/cnn.py
+++ tests/cnn.py
@@ -1,20 +1,21 @@
 from ml.cnn import CNN2D
-import numpy as np
-from ml.data import mnist
 import pickle
-with open("data.txt", "wb") as f:
-    pickle.dump(mnist.train.images[0],f)
+
+with open("label.txt", "rb") as f:
+    label = pickle.load(f)
+with open("data.txt", "rb") as f:
+    image = pickle.load(f)
 
 c = CNN2D(
     layers=2,
-    inp=4,
-    out=4,
-    kernel_size=[1, 1],
-    pool_size=[1, 1],
-    filters=[1, 1],
-    dimensions=[2, 2],
+    inp=784,
+    out=10,
+    kernel_size=[5, 5],
+    pool_size=[2, 2],
+    filters=[32, 64],
+    dimensions=[28, 28],
 )
 
-c.fit(data=np.array([0.1, 0.1, 0.1, 0.1]), labels=np.array([0,0,0,0]), lr=0.001, epochs=1, save_path="./model")
-c.test(data=np.array([0, 0, 0, 0]), labels=[0,0,0,0])
-predicted = c.predict(np.array([0, 0, 0, 0]), transpose=True)
+c.fit(data=image, labels=label, lr=0.001, epochs=1, save_path="./model")
+c.test(data=image, labels=label)
+predicted = c.predict(image, transpose=True)
