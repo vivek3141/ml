e925c2cfe29890c77e90356f79585b75f21ac771
Vivek Verma <vivnps.verma@gmail.com>
2018-11-11 19:59:05 -0800

    Formatting

--- ml/regression/regression.py
+++ ml/regression/regression.py
@@ -1,14 +1,19 @@
 import tensorflow as tf
 import numpy as np
 
+
 class Regression:
     def __init__(self, func):
         self.func = func
+
     def fit(self):
         pass
+
     def predict(self):
         pass
+
     def save(self):
         pass
+
     def load(self):
         pass
