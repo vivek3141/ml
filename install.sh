c3d2b9860fc5e36174d13ba58b03e69349b02c12
Vivek Verma <vivnps.verma@gmail.com>
2018-12-05 18:56:28 -0800

    Updating Files

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 pip install -r requirements.txt
 python3 setup.py install
---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd tests > /dev/null 2>&1
+python3 k_means.py > /dev/null 2>&1
+python3 linear_regression.py > /dev/null 2>&1
+python3 neuralnetwork.py > /dev/null 2>&1
+python3 cnn.py > /dev/null 2>&1
+python3 logistic_regression.py > /dev/null 2>&1
+python3 regression.py > /dev/null 2>&1
+echo "Done Testing"
