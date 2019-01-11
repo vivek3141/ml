bbd63f2b6625291540937dc58f057b41b80197c9
Vivek Verma <vivnps.verma@gmail.com>
2019-01-10 19:40:01 -0800

    Adding Files

--- ml/optimizer/gradient_descent.py
+++ ml/optimizer/gradient_descent.py
@@ -8,5 +8,9 @@ class GradientDescentOptimizer:
         self.num_theta = len(signature(func).parameters)
         print(self.num_theta)
 
-    def optimize(self, learning_rate, steps):
-        pass
+    def optimize(self, learning_rate, steps, init_theta=None):
+        theta = [0 for i in range(self.num_theta)] if init_theta is None
+        for i in range(steps):
+            for i in range(self.num_theta):
+
+
