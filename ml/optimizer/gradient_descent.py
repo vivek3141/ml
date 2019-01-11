c850dca9f49a857936e08693e8fd2f4f53ae9059
Vivek Verma <vivnps.verma@gmail.com>
2019-01-10 19:31:31 -0800

    Adding Tester

--- ml/optimizer/gradient_descent.py
+++ ml/optimizer/gradient_descent.py
@@ -1,6 +1,12 @@
 import numpy as np
+from inspect import signature
 
 
 class GradientDescentOptimizer:
-    def __init__(self):
+    def __init__(self, func):
+        self.func = func
+        self.num_theta = len(signature(func).parameters)
+        print(self.num_theta)
+
+    def optimize(self, learning_rate, steps):
         pass
