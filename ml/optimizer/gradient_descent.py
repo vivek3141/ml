2220f1537cc1b15c66fab21465c8be40dcfd2759
Vivek Verma <vivnps.verma@gmail.com>
2019-01-11 22:08:43 -0800

    Adding Batch Gradient Descent

--- ml/optimizer/gradient_descent.py
+++ ml/optimizer/gradient_descent.py
@@ -1,4 +1,3 @@
-import numpy as np
 from inspect import signature
 
 
@@ -10,14 +9,13 @@ class GradientDescentOptimizer:
     def optimize(self, learning_rate=0.01, steps=1000, init_theta=None, dx=0.0001):
         theta = [0 for _ in range(self.num_theta)] if init_theta is None else init_theta
         for i in range(steps):
-            t_grad = []
+            partials = []
             for t in range(self.num_theta):
-                t_grad.append(learning_rate *
-                              ((self.func(*[theta[x] + dx if t == x else theta[x] for x in range(self.num_theta)])
-                                - self.func(*theta)) / dx)
-                              )
+                theta_dx = [(theta[x] + dx) if t == x else theta[x] for x in range(self.num_theta)]
+                partial = (self.func(*theta_dx) - self.func(*theta)) / dx
+                partials.append(partial)
             for k in range(self.num_theta):
-                theta[k] -= t_grad[k]
+                theta[k] -= learning_rate * partials[k]
             if i % 50 == 0:
                 print(f"Step: {i} Cost {self.func(*theta)}")
         return theta
--- ml/regression/regression.py
+++ ml/regression/regression.py
@@ -27,7 +27,7 @@ class Regression:
         self.x_data = None
         self.m = None
 
-    def fit(self, x, y, init_theta=None, lr=0.001, steps=1000, graph=False, to_print=None):
+    def fit(self, x, y, init_theta=None, lr=0.001, steps=1000, graph=False, to_print=None, batch_size=10):
         """
         Fit the model
         :param x: X data
@@ -37,33 +37,53 @@ class Regression:
         :param steps: Number of steps do go through
         :param graph: Set true to graph function and data
         :param to_print: Print loss and step number every this number
+        :param batch_size: Size of batches for Gradient Descent
         :return: None
         """
         self._check_length(x, y)
+
+        x = np.array(x)
+        y = np.array(y)
+
         if init_theta is None:
             init_theta = [0 for _ in range(self.k)]
+
         self.m = len(x)
+
         try:
             _ = self.func(*init_theta, 0)
         except TypeError:
             raise Error("Initial Theta does not match with function")
+
+        # Define the graph
         self.x = tf.placeholder(tf.float32, shape=[None, 1])
         self.yy = tf.placeholder(tf.float32, shape=[None, 1])
+
         self.theta = [tf.Variable(initial_value=i, dtype=tf.float32) for i in init_theta]
+
         self.y = self.func(*self.theta, self.x)
         self.J = tf.reduce_mean(tf.losses.mean_squared_error(labels=self.yy, predictions=self.y))
+
         self.optim = tf.train.AdamOptimizer(learning_rate=lr).minimize(self.J)
         self.s = tf.Session()
         self.s.run(tf.global_variables_initializer())
+
         for i in range(steps):
-            x_data = [[x[i % self.m]]]
-            y_data = [[y[i % self.m]]]
-            if to_print is not None and i % to_print == 0:
+
+            # Batches for batch gradient descent
+            ind = np.random.choice(len(x), size=batch_size)
+            x_data = np.array([x[ind]]).T
+            y_data = np.matrix(y[ind]).T
+
+            if to_print is not None and i % to_print == 0: \
+                    # Mean Squared Error
                 loss = sum(
                     [self.s.run(self.J, feed_dict={self.x: [[x[n]]], self.yy: [[y[n]]]}) for n in range(self.m)]) / (
-                                   2 * self.m)
+                               2 * self.m)
                 print(f"Step: {i}, Loss:{loss}")
+
             self.s.run(self.optim, feed_dict={self.x: x_data, self.yy: y_data})
+
         if graph:
             x1 = np.linspace(min(x), max(x), 300)
             y1 = list(map(lambda z: self.s.run(self.y, feed_dict={self.x: [[z]]})[0][0], x1))
