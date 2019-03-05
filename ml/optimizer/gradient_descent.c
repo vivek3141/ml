1041ffa3b436c4ba90cfd628ec9ca21ed8d04e5a
Vivek Verma <vivnps.verma@gmail.com>
2019-03-04 16:48:16 -0800

    Updating C File

--- ml/optimizer/gradient_descent.c
+++ ml/optimizer/gradient_descent.c
@@ -7,7 +7,7 @@ int * optimize(void (*func)(int), int learning_rate, int steps, int* theta, int 
         for (int t=0; t < num_theta; t++){
             int * theta_dx = malloc(sizeof(int) * num_theta);
             for( int x = 0; x < num_theta; x++){
-                if(t==x){
+                if(t == x){
                     theta_dx[x] = theta[x] + dx;
                 }
                 else{
@@ -16,11 +16,11 @@ int * optimize(void (*func)(int), int learning_rate, int steps, int* theta, int 
             }
             partials[t] = (*func)(theta_dx) - (*func)(theta) / dx;
         }
-        for k in range(self.num_theta){
-            theta[k] -= learning_rate * partials[k]
+        for(int k =0; k < num_theta; k++){
+            theta[k] -= learning_rate * partials[k];
         }
-        if i % 50 == 0{
-            printf(f"Step: {i} Cost {self.func(*theta)}")
+        if (i % 50 == 0){
+            printf("Step: {%d} Cost {%d}", i, (*func)(theta));
         }
-    return theta
+    return theta;
 }
