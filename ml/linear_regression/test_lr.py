4d0bcb19885bb3e8d508871ede837c351d006bdd
Vivek Verma <vivnps.verma@gmail.com>
2019-03-23 01:31:19 -0700

    Updating C-Python extension

--- ml/linear_regression/test_lr.py
+++ ml/linear_regression/test_lr.py
@@ -2,12 +2,18 @@ import linear_regression
 import numpy as np
 import matplotlib.pyplot as plt
 from ml.linear_regression import LinearRegression
+import time
 
 x = sorted([0.1, 2.3, 6.3, 9.2, 1.6, 2.3, 5.1, 7.2, 4.3, 1.5, 6.3, 8.9, 3.2, 3.5, 5.6])
 y = list(map(lambda x: 0.3*x + 0.2, x))
 
+start_time = time.time()
+#theta = LinearRegression()
+#theta.fit(np.array(x), np.array(y), lr=0.01)
+#theta = theta.theta
 theta = linear_regression.fit(x, y, 0.01, 1000, [1, 1], len(x))
-theta = linear_regressio
+
+print("--- %s seconds ---" % (time.time() - start_time))
 
 x_line = np.array([x[0], x[-1]])
 y_line = theta[0] * x_line + theta[1]
