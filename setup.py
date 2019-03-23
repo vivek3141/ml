382d04448ea86beee06156d6eb053ca09ef69979
Vivek Verma <vivnps.verma@gmail.com>
2019-03-23 10:51:38 -0700

    Linear Regression C Link

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 from setuptools import find_packages
 from setuptools import Extension
 
-gradient_descent = Extension('gradient_descent', sources=['ml/optimizer/gradient_descent.c'])
-#linear_regression = Extension('linear_regression', sources=['ml/linear_regression/linear_regression.c'])
+gradient_descent = Extension('gradient_descent', sources=[
+                             'ml/optimizer/gradient_descent.c'])
+linear_regression = Extension('linear_regression', sources=[
+                              'ml/linear_regression/linear_regression.c'])
 
 
 setup(
@@ -17,7 +19,7 @@ setup(
     description="The easiest way to do machine learning",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    ext_modules=[gradient_descent],
+    ext_modules=[gradient_descent, linear_regression],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2",
