7731b0b0d36c76f2ed0f607da9c755dfe6965997
Vivek Verma <vivnps.verma@gmail.com>
2020-01-31 10:10:13 -0800

    Adam Optimizer

--- ml/optimizer/adam.py
+++ ml/optimizer/adam.py
@@ -40,32 +40,3 @@ class AdamOptimizer:
                         for x in range(self.num_theta)]
             partial = (self.func(*theta_dx) - self.func(*theta)) / dx
             partials.append(partial)
-        return np.array(partials)
-
-
-# Data
-x1 = [0.00, 4.48, 8.96, 13.44, 17.92, 22.41, 26.89, 31.37, 35.85, 40.33, 44.81]
-y1 = [0.00, 2.89, 5.14, 6.74, 7.71, 8.03, 7.71, 6.74, 5.14, 2.89, 0.00]
-
-
-# Defining Cost Function to optimize
-def cost_function(a, b, c):
-    j = 1 / len(x1)
-    sigma = 0
-    for i in range(len(x1)):
-        sigma = sigma + (y1[i] - func(a, b, c, x1[i])) ** 2
-    j = j * sigma
-    return j
-
-
-# Function to fit the points
-def func(a, b, c, x):
-    return a * (x ** 2) + b * x + c
-
-
-# Optimize
-g = AdamOptimizer(cost_function)
-theta = g._optimize_python()
-
-# Graph the function by using the theta learnt
-#graph_function_and_data(lambda x: func(*theta, x), x_data=x1, y_data=y1)
