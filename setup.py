c4bea3715f801d858bc735219fec3576ac3bb8a2
Vivek Verma <vivnps.verma@gmail.com>
2020-02-03 22:13:49 -0800

    New Version 2.3

--- setup.py
+++ setup.py
@@ -3,27 +3,29 @@ from setuptools import find_packages
 from setuptools import Extension
 
 gradient_descent = Extension('gradient_descent', sources=[
-                            'ml/optimizer/gradient_descent.c'])
+    'ml/optimizer/gradient_descent.c'])
 linear_regression = Extension('lin_reg', sources=[
-                              'ml/linear_regression/linear_regression.c'])
+    'ml/linear_regression/linear_regression.c'])
+adam = Extension('adam', sources=[
+    'ml/optimizer/adam.c'])
 
 
 setup(
-    name='ml-python',
-    version='2.2.2',
-    author="Vivek Verma",
-    packages=find_packages(),
-    author_email="vivekverma3141@gmail.com",
-    url="https://github.com/vivek3141/ml",
-    license='MIT',
-    description="The easiest way to do machine learning",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    ext_modules=[gradient_descent, linear_regression],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 2",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
-)
+        name='ml-python',
+        version='2.3',
+        author="Vivek Verma",
+        packages=find_packages(),
+        author_email="vivekverma3141@gmail.com",
+        url="https://github.com/vivek3141/ml",
+        license='MIT',
+        description="The easiest way to do machine learning",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        ext_modules=[gradient_descent, linear_regression],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 2",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ]
+        )
